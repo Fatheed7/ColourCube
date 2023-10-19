34ec6dbe9c96fe2430fea9cf8cab76643188750b
Fatheed7 <dunton.jason@gmail.com>
2023-10-19 10:40:33 +0000

    Ran py code through linter

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
+
 
 @app.route('/<int:h>/<int:w>/<int:r>/<int:g>/<int:b>')
 @app.route('/<int:h>/<int:w>/<int:r>/<int:g>/<int:b>/')
@@ -28,18 +30,26 @@ def generate_image(h, w, r, g, b):
 
     return send_file(image_stream, mimetype='image/png')
 
+
 def check_values(h, w, r, g, b):
     switch = {
         h > 150: "Please enter a height below 100 pixels",
         w > 150: "Please enter a width below 100 pixels",
-        not isinstance(r, int) or not isinstance(g, int) or not isinstance(b, int): "Please enter valid integers for the red, green, and blue values.",
-        not (0 <= r <= 255) or not (0 <= g <= 255) or not (0 <= b <= 255): "Please enter red, green, and blue values between 0 and 255."
+        not isinstance(r, int) or not isinstance(g, int) or not isinstance(
+            b, int): (
+                "Please enter valid integers for the"
+                "red, green, and blue values."),
+        not (0 <= r <= 255) or not (0 <= g <= 255) or not (
+            0 <= b <= 255): ("Please enter red, green, and blue"
+                             "values between 0 and 255.")
     }
     return switch.get(True, None)
+
 
 @app.route('/<path:invalid_route>')
 def invalid_route(invalid_route):
     return render_template("404.html")
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
